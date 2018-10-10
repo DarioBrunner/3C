1de00d37cd524345d63d25737c91da53dd325af8
Dario Brunner <Brunnerdario@googlemail.com>
2018-10-10 17:04:20 +0200

    selectet menu for roles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_10_053500) do
+ActiveRecord::Schema.define(version: 2018_10_10_142708) do
+
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "channels", force: :cascade do |t|
     t.integer "typ"
